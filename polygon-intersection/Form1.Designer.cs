d6b88e46dd8d7e5728c2448d6da102116e73f8d5
Andrew Vaimon <niko142000@mail.ru>
2021-10-20 11:19:36 +0300

    basics

---
+++ polygon-intersection/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace polygon_intersection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.textView = new System.Windows.Forms.Label();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(540, 12);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(210, 51);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Нарисовать полигоны";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // textView
+            // 
+            this.textView.AutoSize = true;
+            this.textView.BackColor = System.Drawing.SystemColors.Window;
+            this.textView.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textView.Location = new System.Drawing.Point(459, 77);
+            this.textView.Name = "textView";
+            this.textView.Size = new System.Drawing.Size(388, 38);
+            this.textView.TabIndex = 1;
+            this.textView.Text = "Нарисуйте первый полигон";
+            this.textView.Visible = false;
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1300, 900);
+            this.canvas.TabIndex = 2;
+            this.canvas.TabStop = false;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 900);
+            this.Controls.Add(this.textView);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.canvas);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Пересекалка";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label textView;
+        private System.Windows.Forms.PictureBox canvas;
+    }
+}
+
---
+++ polygon-intersection/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polygon_intersection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+        }
+
+        bool isDrawingMode = false;
+        bool isClearingMode = false;
+        bool isFirstDrawn = false;
+        List<Point> polygon1Points = new List<Point>();
+        List<Point> polygon2Points = new List<Point>();
+        Pen blackPen = new Pen(Color.Black, 3);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Graphics g;
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (isClearingMode)
+            {
+                isDrawingMode = false;
+                isClearingMode = false;
+                canvas.Image = new Bitmap(1300, 900);
+                polygon1Points.Clear();
+                polygon2Points.Clear();
+                btnDraw.Text = "Нарисовать полигоны";
+                textView.Visible = false;
+            }
+            else
+            {
+                isDrawingMode = true;
+                isClearingMode = true;
+                btnDraw.Text = "Очистить";
+                textView.Text = "Нарисуйте 1 полигон";
+                textView.Visible = true;
+            }
+        }
+        
+        private void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isDrawingMode)
+            {
+                if (isFirstDrawn)
+                {
+                    drawPolygon(e, ref polygon2Points);
+                }
+                else
+                {
+                    drawPolygon(e, ref polygon1Points);
+                }
+            }
+        }
+
+        void drawPolygon(MouseEventArgs e, ref List<Point> polygonPoints)
+        {
+            if (polygonPoints.Count > 0)
+            {
+                if (polygonPoints.Count == 1)
+                {
+                    g.Clear(Color.White);
+                    if (isFirstDrawn)
+                    {
+                        for(int i = 0; i < polygon1Points.Count; i++)
+                        {
+                            g.DrawLine(blackPen, polygon1Points[i], polygon1Points[(i+1) % polygon1Points.Count]);
+                        }
+                    }
+                }
+                if (Math.Abs(polygonPoints[0].X - e.X) * Math.Abs(polygonPoints[0].Y - e.Y) < 25)
+                {
+                    g.DrawLine(blackPen, polygonPoints.Last(), polygonPoints[0]);
+                    if (isFirstDrawn)
+                    {
+                        isFirstDrawn = false;
+                        textView.Text = "Нарисуйте 1 полигон";
+                        textView.Visible = false;
+                        isDrawingMode = false;
+                        intersectPolygons();
+                    }
+                    else
+                    {
+                        isFirstDrawn = true;
+                        textView.Text = "Нарисуйте 2 полигон";
+                    }
+                    return;
+                }
+                g.DrawLine(blackPen, polygonPoints.Last(), e.Location);
+                polygonPoints.Add(e.Location);
+            }
+            else
+            {
+                polygonPoints.Add(e.Location);
+                g.FillEllipse(blackBrush, e.X - 2, e.Y - 2, 5, 5);
+            }
+        }
+
+        void intersectPolygons()
+        {
+
+        }
+    }
+}
